b4a3462d7cf443a14bc2018400dd3aeb5e39e069
ArpanSankesh <arpansankesh830@gmail.com>
2024-11-02 16:54:09 +0530

    Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import Login from "./components/Auth/Login"
+import { setLocalStorage } from "./utils/LocalStorage";
 // import EmployeeDashboard from "./components/DashBoard/EmployeeDashboard";
 // import AdminDashboard from "./components/DashBoard/AdminDashboard";
+import { useEffect } from "module";
+import {setLocalStorage} from './utils/LocalStorage'
 
 
 const App = () => {
+useEffect(() => {
+  setLocalStorage()
+})
+
+
   return (
     <>
       <Login />
