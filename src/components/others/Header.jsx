1fb588dff14f99f2ce887e7ee44046eb632c5f24
ArpanSankesh <arpansankesh830@gmail.com>
2024-11-05 13:55:25 +0530

    Login

--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -1,12 +1,25 @@
 /* eslint-disable react/prop-types */
 
-const Header = ({data}) => {
-  console.log(data);
+// import { useState } from "react"
+
+const Header = () => {
+  // const [username, setUsername] = useState('')
+
+  // if(!username){
+  //   setUsername('Admin')
+  // }else{
+  //   setUsername(data.name)
+  // }
+
+  const logOutUser = () => {
+    localStorage.setItem('loggedInUser','')
+    window.location.reload()
+  }
   
   return (
     <div className=" text-white flex justify-between items-center px-3">
-        <h1 className="text-xl font-medium">Hello, <br /> <span className="text-3xl font-semibold">{data.name} 👋</span></h1>
-        <button className="bg-red-700 px-5 py-2 rounded-md">Log out</button>
+        <h1 className="text-xl font-medium">Hello, <br /> <span className="text-3xl font-semibold">username 👋</span></h1>
+        <button onClick={logOutUser} className="bg-red-700 px-5 py-2 rounded-md">Log out</button>
     </div>
   )
 }
