ba32e2dc4b252d6ec5046e60b06d6350aa7f5dea
ArpanSankesh <arpansankesh830@gmail.com>
2024-11-05 14:03:36 +0530

    Login

--- src/components/TaskList/FailedTask.jsx
+++ src/components/TaskList/FailedTask.jsx
@@ -1,25 +1,20 @@
 /* eslint-disable react/prop-types */
-
-const FailedTask = ({data}) => {
+const FailedTask = ({ data }) => {
   return (
     <div className=" flex-shrink-0 rounded-lg h-full w-[350px] bg-yellow-500 p-5 ">
       <div className="flex items-center justify-between ">
         <h2 className="bg-red-500 px-3 p-1 rounded-md text-sm font-bold">
-        {data.category}
+          {data.category}
         </h2>
         <h3 className="text-sm font-semibold">{data.date}</h3>
       </div>
-      <h1 className="mt-10 text-2xl font-bold">
-      {data.title}
-      </h1>
-      <p className="text-sm mt-3">
-      {data.description}
-      </p>
+      <h1 className="mt-10 text-2xl font-bold">{data.title}</h1>
+      <p className="text-sm mt-3">{data.description}</p>
       <div className="flex justify-between mt-5">
         <button className="p-2 px-4 bg-red-700 rounded-md">Failed</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FailedTask
+export default FailedTask;
