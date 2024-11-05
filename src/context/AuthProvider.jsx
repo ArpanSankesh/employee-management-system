a38cbc198425eed324c584b13a6024382e54b638
ArpanSankesh <arpansankesh830@gmail.com>
2024-11-05 18:23:52 +0530

    Login

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,27 +1,23 @@
-import { createContext, useEffect, useState } from "react"
-import { getLocalStorage, setLocalStorage } from "../utils/LocalStorage"
+import { createContext, useEffect, useState } from "react";
+import { getLocalStorage, setLocalStorage } from "../utils/LocalStorage";
 
 // eslint-disable-next-line react-refresh/only-export-components
-export const AuthContext = createContext()
+export const AuthContext = createContext();
 
 // eslint-disable-next-line react/prop-types
-const AuthProvider = ({children}) => {
-    const [userData, setUserData] = useState([])
-    useEffect(() => {
-      setLocalStorage()
-        const { employees, admin } = getLocalStorage();
-        setUserData({ employees, admin });
-    }, []);
-    
-    
+const AuthProvider = ({ children }) => {
+  const [userData, setUserData] = useState([]);
+  useEffect(() => {
+    setLocalStorage();
+    const { employees, admin } = getLocalStorage();
+    setUserData({ employees, admin });
+  }, []);
 
   return (
     <div>
-        <AuthContext.Provider value = {userData}>
-        {children} 
-        </AuthContext.Provider>
+      <AuthContext.Provider value={userData}>{children}</AuthContext.Provider>
     </div>
-  )
-}
+  );
+};
 
-export default AuthProvider
+export default AuthProvider;
